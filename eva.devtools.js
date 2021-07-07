f58e8e364f51eaa933d15807b277c2677f644b0c
manyyuri <ljy085@126.com>
2021-07-07 17:06:22 +0800

    修正eva.pannel.html为eva.panel.html

--- eva.devtools.js
+++ eva.devtools.js
@@ -3,7 +3,7 @@
 chrome.devtools.panels.create(
   "Eva Panel",
   "img/icon.png",
-  "chrome-extension/eva.pannel.html",
+  "chrome-extension/eva.panel.html",
   function (panel) {
     console.log("自定义面板创建成功！"); // 注意这个log一般看不到
   }
